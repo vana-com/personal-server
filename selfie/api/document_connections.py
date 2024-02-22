6efa8c4a0df1f1443cbb6470d4b759579c3ae414
Kahtaf Alam <kahtaf@gmail.com>
2024-02-22 16:55:19 -0500

    Save the document and document connection to the DB and embedded document to the vector DB

--- selfie/api/document_connections.py
+++ selfie/api/document_connections.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel
 from starlette import status
 
 from selfie.connectors.factory import ConnectorFactory
+from selfie.database import DataManager, DocumentModel
+from selfie.embeddings import DataIndex
 
 router = APIRouter()
 
@@ -17,7 +19,25 @@ class DocumentConnectionRequest(BaseModel):
 @router.post("/document-connections", status_code=status.HTTP_201_CREATED)
 async def create_document_connection(request: DocumentConnectionRequest):
     connector_instance = ConnectorFactory.get_connector(connector_name=request.connector_id)
-    selfie_documents = connector_instance.load_document(request.configuration)
-    # TODO: save selfie_documents to SQLite
-    embedding_documents = connector_instance.transform_for_embedding(request.configuration, selfie_documents)
-    # TODO: save embedding_documents to Vector DB
+    connector_instance.validate_configuration(configuration=request.configuration)
+    document_dtos = connector_instance.load_document(request.configuration)
+
+    if len(document_dtos) > 0:
+        # Save document connection
+        document_connection = DataManager().add_document_connection(
+            request.connector_id,
+            request.configuration
+        )
+        # Save documents
+        for document_dto in document_dtos:
+            document_model = DocumentModel.create(
+                document_connection=document_connection,
+                content=document_dto.content,
+                content_type=document_dto.content_type,
+                name=document_dto.name,
+                size=document_dto.size
+            )
+
+        embedding_documents = connector_instance.transform_for_embedding(request.configuration, document_dtos)
+        # Save embedding_documents to Vector DB
+        await DataIndex("n/a").index(embedding_documents, extract_importance=False)
