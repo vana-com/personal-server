330ef306a77e253a5fba8ec1468e7deaa14b0e99
Tim Nunamaker <tim@vana.com>
2024-03-01 16:32:41 -0600

    chore: simplify setup procedure

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -2,17 +2,22 @@
 
 set -e
 
-# Step 1: Build Next.js App
-echo "Building Next.js app..."
+echo "Building Selfie UI..."
 cd selfie-ui
-yarn install --frozen-lockfile
-NODE_ENV=production yarn run build
 
-# Step 2: Move the out/ directory to FastAPI static file serving directory
-echo "Moving built files to FastAPI app..."
+if command -v yarn &> /dev/null; then
+    yarn install --frozen-lockfile --non-interactive
+    yarn run build
+else
+    npm install --no-prompt --cache .npm
+    # npm ci --cache .npm # requires a lockfile
+    NODE_ENV=production npm run build
+fi
+
+echo "Moving built files to Selfie API app..."
 cd ..
 rm -rf selfie/web
 mkdir -p selfie/web
 cp -r selfie-ui/out/* selfie/web/
 
-echo "Next.js app built and integrated with FastAPI."
+echo "Selfie UI built and integrated with API."
