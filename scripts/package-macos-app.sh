267b5969b70f88babf389b6ad2d15e7d192ec9c0
Tim Nunamaker <tim.nunamaker@gmail.com>
2024-03-12 14:07:58 -0500

    Attempt to notarize macOS app

---
+++ scripts/package-macos-app.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Variables
+APP_NAME="Selfie"
+BUNDLE_IDENTIFIER="com.vana.selfie"
+VERSION="0.1.0"
+EXECUTABLE_NAME="selfie"
+DIST_DIR="dist/selfie"
+APP_DIR="${DIST_DIR}/${APP_NAME}.app"
+CONTENTS_DIR="${APP_DIR}/Contents"
+MACOS_DIR="${CONTENTS_DIR}/MacOS"
+RESOURCES_DIR="${CONTENTS_DIR}/Resources"
+INFO_PLIST="${CONTENTS_DIR}/Info.plist"
+
+# Create .app directory structure
+mkdir -p "${MACOS_DIR}"
+mkdir -p "${RESOURCES_DIR}"
+
+# Move the executable
+mv "${DIST_DIR}/${EXECUTABLE_NAME}" "${MACOS_DIR}/"
+
+# Move resources and libraries
+mv "${DIST_DIR}/_internal" "${RESOURCES_DIR}/"
+#mv "${DIST_DIR}/_internal/data" "${RESOURCES_DIR}/data"
+
+# Create Info.plist
+cat <<EOF > "${INFO_PLIST}"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CFBundleExecutable</key>
+    <string>${EXECUTABLE_NAME}</string>
+    <key>CFBundleIdentifier</key>
+    <string>${BUNDLE_IDENTIFIER}</string>
+    <key>CFBundleName</key>
+    <string>${APP_NAME}</string>
+    <key>CFBundleVersion</key>
+    <string>${VERSION}</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+</dict>
+</plist>
+EOF
+
+echo "${APP_NAME}.app bundle has been created."
