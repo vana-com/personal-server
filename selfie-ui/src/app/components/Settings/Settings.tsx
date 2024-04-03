fe0f9905933a5c321035c525332aee7f198ad5a1
Tim Nunamaker <tim@vana.com>
2024-04-03 15:07:45 -0500

    Escape apostrophe

--- selfie-ui/src/app/components/Settings/Settings.tsx
+++ selfie-ui/src/app/components/Settings/Settings.tsx
@@ -372,7 +372,7 @@ const Settings = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
             </svg>
-            <span>Heads up, you've selected llama.cpp - please ensure you have enough system RAM to load the configured model!</span>
+            <span>Heads up, you&apos;ve selected llama.cpp - please ensure you have enough system RAM to load the configured model!</span>
           </div>
       )}
 
